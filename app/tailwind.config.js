4d5ce3a49c5f5c9d6f25cefe1b233c4863f01167
chechna9 <ja_bouchouareb@esi.dz>
2022-06-10 03:16:36 +0100

    tailwindCss config

---
+++ app/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}",],
+  theme: {
+    extend: {},
+    colors : {
+      'prim1':"#231942",
+      'prim2':"#42307D",
+      'sec':"#5e548e",
+
+    }
+  },
+  plugins: [],
+}
