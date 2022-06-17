6bded20f738d1eda9142cb6ef7b8ca2888ad8718
chechna9 <ja_bouchouareb@esi.dz>
2022-06-17 10:23:15 +0100

    navbar done

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/navbar';
 
 function App() {
   return (
-   <Navbar/>
+    <>
+    <Navbar/>
+    </>
   );
 }
 
--- app/src/components/navbar.jsx
+++ app/src/components/navbar.jsx
@@ -1,7 +1,41 @@
+import React from "react";
+import homeIcon from "../assets/homeIcon.svg";
+import projectIcon from "../assets/projectIcon.svg";
+import contactIcon from "../assets/contactIcon.svg";
 const Navbar = () => {
-    return ( 
-        <p>Navbar</p>
-     );
-}
- 
+  const navLinkStyle =
+    "hidden md:block hover:scale-125 transition-all duration-300";
+  const iconStyle = "md:hidden w-12";
+  return (
+    <div className="bg-sec md:px-52 py-4 rounded-b-3xl ">
+      <ul className="flex flex-row justify-around  font-bold text-white ">
+        <li className={navLinkStyle}>
+          {" "}
+          <a href="#home">Home</a>
+        </li>{" "}
+        <a href="#home">
+          {" "}
+          <img src={homeIcon} className={iconStyle} alt="" />
+        </a>
+        <li className={navLinkStyle}>
+          {" "}
+          <a href="#projects">Projects</a>
+        </li>
+        <a href="#projects">
+          {" "}
+          <img src={projectIcon} className={iconStyle} alt="" />
+        </a>
+        <li className={navLinkStyle}>
+          {" "}
+          <a href="#contactUs">Contact us</a>
+        </li>
+        <a href="#contactUs">
+          {" "}
+          <img src={contactIcon} className={iconStyle} alt="" />
+        </a>
+      </ul>
+    </div>
+  );
+};
+
 export default Navbar;
